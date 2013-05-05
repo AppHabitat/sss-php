b6bf7ffdc03518cc2e516c1dd08a34b6cb590701
Mike Holly <mikejholly@gmail.com>
2013-05-05 16:04:35 -0700

    Create ScreenshotShark.php

---
+++ ScreenshotShark.php
@@ -0,0 +1,41 @@
+<?php
+
+class ScreenshotShark {
+
+  const API_URL = 'http://www.screenshotshark.com/capture';
+
+  protected $options;
+  protected $key;
+  protected $secret;
+
+  /**
+   * Create a new instance of this class.
+   * @param string $key    The service API key
+   * @param string $secret Your secret key
+   */
+  public function __construct($key, $secret) {
+    $this->key = $key;
+    $this->secret = $secret;
+  }
+
+  /**
+   * Create a SHA1 HMAC token.
+   * @param  array $opts The screenshot options
+   * @return string      The hash string
+   */
+  public function createHash($opts) {
+    $data = sprintf('%s:%s:%s', $opts['time'], $opts['url'], $opts['op']);
+    return hash_hmac('sha1', $data, $this->secret);
+  }
+
+  /**
+   * Return the final URL for this screenshot.
+   * @param  array  $opts The screenshot options
+   * @return string       The screenshot URL
+   */
+  public function getScreenshotUrl($opts) {
+    $opts['token'] = $this->createHash($opts);
+    $query = http_build_query($opts, NULL, '&');
+    return self::API_URL . '?' . $query;
+  }
+}
